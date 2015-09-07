1d2a227af908ab0e457e9da1e0f31c3066e5fba4
rafaelbetatester <rafaelbetatester@gmail.com>
2015-09-07 11:30:17 -0300

    mudanças no codigo

--- eventree.cpp
+++ eventree.cpp
@@ -1,40 +1,72 @@
 #include <stdio.h>
 #include <vector>
+#include <string.h>
 #include <queue>
 #define N 100 + 5
 using namespace std;
-vector<int> vizinho[N];
-int contador[N];
+int parent[N];
+vector<int> family[N];
+vector<int> edge[N];
+queue<int> root;
+bool marked[N];
+int descendents[N];
+
+int numberDescendents(int x)
+{
+  int sz = family[x].size();
+  if (sz == 0) return descendents[x] = 1;
+  for (int i = 0; i < sz; i++)
+    {
+      int son = family[x][i];
+      descendents[x]+=numberDescendents(son);
+    }
+  return descendents[x];
+}
+
 int main()
 {
+  for (int i = 1; i <= N; i++)
+    descendents[i] = 1;
   int n,m;
   scanf("%d%d",&n,&m);
   for (int i = 1; i <= m; i++)
     {
       int x,y;
       scanf("%d%d",&x,&y);
-      deg[x]++;
-      deg[y]++;
-      vizinho[x].push_back(y);
-      vizinho[y].push_back(x);
+      edge[x].push_back(y);
+      edge[y].push_back(x);
     }
-  queue <int> folhas;
-  for (int i = 1; i <= n; i++)
-    if (deg[i] == 1)
-      folhas.push(i);
-  while(!folhas.empty())
+  root.push(1);
+  while(!root.empty())
     {
-      int atual = folhas.front();
-      folhas.pop();
-      int sz = vizinho[atual].size();
+      int r = root.front();
+      root.pop();
+      marked[r] = true;
+      int sz = edge[r].size();
       for (int i = 0; i < sz; i++)
 	{
-	  int amigo = vizinho[atual][i];
-	  deg[amigo]--;
-	  contador[amigo]+= contador[atual] + 1;
-	  if (deg[amigo] == 1)
-	    folhas.push(amigo);
+	  int son = edge[r][i];
+	  if (!marked[son])
+	    {
+	      root.push(son);
+	      parent[son] = r;
+	      family[r].push_back(son);
+	    }
 	}
     }
+  
+  
+  //for (int i = 1; i <= n; i++)
+  //printf("%d %d\n",i, parent[i]);
+  int total = numberDescendents(1);
+  //
+  int conta = -1;
+  //printf("%d %d\n",i, descendents[i]);
+  for (int i = 1; i <= n; i++)
+    if (descendents[i]%2==0)
+      conta++;
+  printf("%d\n",conta);
   return 0;
+
+  
 }
