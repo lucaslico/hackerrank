e0d7f637dd130606eabfcf8b8038e48f150b0f8c
rafaelbetatester <rafaelbetatester@gmail.com>
2015-09-08 20:30:37 -0300

    The Sum of Powers

---
+++ functionalprogramming/sumsofpowers.lisp
@@ -0,0 +1,11 @@
+(defun numberpowers(x n minimo)
+  (cond ((= x 0) 1)
+	((< x (* minimo minimo)) 0)
+	((< x 0) 0)
+	(t (+ (numberpowers (- x (expt minimo n)) n (1+ minimo))
+	      (numberpowers x n (1+ minimo))))))
+
+(defun main()
+  (let*((x (parse-integer(read-line)))
+	(n (parse-integer(read-line))))
+    (format t "~A~%" (numberpowers x n 1))))
